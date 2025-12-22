6053cbbc385e734dc5d687fcdaf809205f06757a
Dhruva <dhruva_kolikineni@dexcom.com>
2025-12-21 20:17:16 -0800

    Add competitive landscape section to investor site

--- investor_site/src/components/pitch/Risks.jsx
+++ investor_site/src/components/pitch/Risks.jsx
@@ -164,7 +164,7 @@ export default function Risks() {
         </motion.div>
 
         {/* Competitive Landscape Section */}
-        <div className="mt-12">
+        <div className="mt-20">
           <CompetitiveLandscape />
         </div>
       </div>
